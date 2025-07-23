a3beb15b6a768d9b59344b00f62220442d581d3f
VR <vladrymarchuk@gmail.com>
2025-07-23 12:52:18 +0000

    feat: add models, admin

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from library.models import (
+    User,
+    Author,
+    Genre,
+    Book,
+    Reservation,
+    Purchase,
+    LikedBook,
+
+)
+
+admin.site.register(User)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book)
+admin.site.register(Reservation)
+admin.site.register(Purchase)
+admin.site.register(LikedBook)
--- library/models.py
+++ library/models.py
@@ -1,3 +1,146 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from config import settings
+
+
+class StatusReservation(models.TextChoices):
+    PENDING = "pending"
+    ACTIVE = "active"
+    COMPLETED = "completed"
+    CANCELLED = "cancelled"
+
+class PaymentReservation(models.TextChoices):
+    PENDING = "pending"
+    PAID = "paid"
+    FAILED = "failed"
+    CANCELLED = "cancelled"
+
+class User(AbstractUser):
+    pass
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+        unique_together = ("first_name", "last_name")
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+
+class Genre(models.Model):
+    genre_name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ["genre_name"]
+
+    def __str__(self):
+        return self.genre_name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ManyToManyField(
+        Author,
+        related_name="books"
+    )
+    genres = models.ManyToManyField(Genre, related_name="books",)
+    publication_year = models.DateField()
+    description = models.TextField()
+    quantity = models.PositiveIntegerField(default=1,)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover_image_url = models.ImageField(blank=True, null=True)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        authors_list = [author.full_name() for author in self.author.all()]
+        return f"{self.title}: ({', '.join(authors_list)})"
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="reservations",
+    )
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="reservations",
+    )
+
+    reservation_date = models.DateTimeField(auto_now_add=True,)
+    due_date = models.DateField()
+    status = models.CharField(
+        max_length=20,
+        choices=StatusReservation.choices,
+        default="pending",
+    )
+
+    class Meta:
+        unique_together = ("user", "book")
+        ordering = ["-reservation_date"]
+
+    def __str__(self):
+        return f"The {self.book.title} reserved by {self.user}"
+
+
+class Purchase(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="purchases",
+    )
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="purchases",
+    )
+    purchase_date = models.DateTimeField(auto_now_add=True,)
+    quantity = models.PositiveIntegerField(default=1)
+    total_amount = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+    )
+    payment_status = models.CharField(
+        max_length=20,
+        choices=PaymentReservation.choices,
+        default="pending",
+    )
+
+    class Meta:
+        ordering = ["-purchase_date"]
+
+    def __str__(self):
+        return f"Purchase {self.book.title} by {self.user}"
+
+
+class LikedBook(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="liked_books",
+    )
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="liked_by_users",
+    )
+    added_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "book")
+        ordering = ["-added_date"]
+
+    def __str__(self):
+        return f"{self.user} liked {self.book.title}"
